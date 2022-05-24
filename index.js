e89cd5b2b57163c9c6b1c950ceffa512ef6cf4a6
Jason Rothgeb <jrothgeb@gmail.com>
2022-05-23 21:07:12 -0500

    commit tree 5be2192733a02178de321ec0a2dda2173e82ea0a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+//const names = ["Lucy", "Charlie", "Linus"];
+//const event = "birthday";
+
+function writeCards(names,event) {
+  let collectMessages =[];
+  for (let i = 0; i < names.length; i++) {
+    collectMessages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    
+  }
+  return collectMessages;
+}
+
+
+
+
+
+function countDown(startingInt) {
+  while(startingInt >=0) {
+  console.log(startingInt);
+  startingInt--;  
+  }
+}
